8827c2c38414e1f319f6d891a53ce2146e57d63e
yazan baroud <yazaniphone2@gmail.com>
2024-12-30 17:23:43 +0200

    errorHandling

---
+++ Controllers/BuggyController.cs
@@ -0,0 +1,38 @@
+using System;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController(DataContext context): BaseApiController
+{   
+    [Authorize]
+    [HttpGet("auth")]
+    public ActionResult<string> GetAuth()
+    {
+        return "secret text";
+    }
+
+    [HttpGet("not-found")]
+    public ActionResult<AppUser> GetNotFound()
+    {
+        var thing = context.Users.Find(-1);
+        if(thing == null) return NotFound();
+        return thing;
+    }
+
+    [HttpGet("server-error")]
+    public ActionResult<AppUser> GetServerError()
+    {  
+        var thing = context.Users.Find(-1) ?? throw new Exception("A bad thing was happen");
+        return thing;
+    }
+
+    [HttpGet("bad-request")]
+    public ActionResult<AppUser> GetBadRequest()
+    {  
+        return BadRequest("this was not a good request");
+    }
+}
--- DTOs/RegisterDto.cs
+++ DTOs/RegisterDto.cs
@@ -6,8 +6,9 @@ namespace API.DTOs;
 public class RegisterDto
 {
     [Required]
-    public required string UserName { get; set; }
+    public string UserName { get; set; } = string.Empty;
 
     [Required]
-    public required string Password { get; set; }
+    [StringLength(8, MinimumLength =4)]
+    public string Password { get; set; } = string.Empty;
 }
